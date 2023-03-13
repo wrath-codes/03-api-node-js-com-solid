020d314ce35d358fbe3bffac2b0409f0bc94dd53
Raphael <raphael@wrathcodes.com>
2023-03-13 14:17:09 -0300

    config: Setup environment variables, eslint, .npmrc and path mapping.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import { app } from "./app";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
+app
+  .listen({
     host: '0.0.0.0',
-    port: 6969
-}).then(() => {
-    console.log('👾 HTTP Server Running!');
-})
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`👾 HTTP Server Running on port ${env.PORT}!`)
+  })
